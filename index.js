5d88bc94d0d9ee8a61838f5adb20296d729b267e
Rachel Wey <rachelwey96@outlook.com>
2018-10-31 07:24:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var price = Math.ceil(Math.random()*100)
+ var price = parseInt(Math.ceil(Math.random()*100))
  cart.push({[item] : price})
  console.log(`${item} has been added to your cart.`)
  return cart
